9365f0ad4175f0742cc3039db0876c0ecc4847e1
RandyRomero <a.ya.miheev@gmail.com>
2017-03-17 18:19:04 +0300

    Make todo-list and make script to open log file

---
+++ greatest.py
@@ -0,0 +1,17 @@
+#!python3
+
+'''Exercise from Automate the Boring Stuff With Python. Find the folder in a directory tree that has the greatest number of files or the folder that uses the most disk space.'''
+
+#def greatestNumber(path):
+#def mostDiskSpace(path):
+
+logFile = open('D:\\YandexDisk\\Studies\\Python\\Chapter 9\\GreatestNumberOfFiles\\logfile.txt', 'w')
+logFile.write('Log file has created. Program has started')
+
+
+
+#TODO: ask user what does he want: folder with greatest number. Store answer as boolean
+#TODO: ask user about directory
+#TODO: check whether directory valind and exists
+#TODO: launch on of two engines
+#TODO: print out the result 
