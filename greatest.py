bf72eba27de5c1899bf04c0dbea4db80fc97cd22
RandyRomero <a.ya.miheev@gmail.com>
2017-03-17 19:37:31 +0300

    add menu where script ask user which of two mode he would like to choose; add gitignore file

--- greatest.py
+++ greatest.py
@@ -6,8 +6,14 @@
 #def mostDiskSpace(path):
 
 logFile = open('D:\\YandexDisk\\Studies\\Python\\Chapter 9\\GreatestNumberOfFiles\\logfile.txt', 'w')
-logFile.write('Log file has created. Program has started')
+logFile.write('Log file has created. Program has started.\n\n')
 
+print('Hi there! Let\'s quickly get down to deal. \nIf you want to find a folder that contains the greatest number of files, press 1. \nIf you want to find a folder that uses the most disk space, press 2. \nYour answer is: ')
+logFile.write('Hi there! Let\'s quickly get down to deal. \nIf you want to find a folder that contains the greatest number of files, press 1. \nIf you want to find a folder that uses the most disk space, press 2. Your answer is: ')
+
+mode = input()
+print('You have chosen ' + mode + '.')
+logFile.write('User has chosen ' + mode + '.\n\n')
 
 
 #TODO: ask user what does he want: folder with greatest number. Store answer as boolean
