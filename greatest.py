3ba5faea8d42859db4a68e91c5bb1e64d4f398de
RandyRomero <a.ya.miheev@gmail.com>
2017-03-17 19:49:21 +0300

    make sure that user chose nithing but 1 or 2

--- greatest.py
+++ greatest.py
@@ -8,12 +8,20 @@
 logFile = open('D:\\YandexDisk\\Studies\\Python\\Chapter 9\\GreatestNumberOfFiles\\logfile.txt', 'w')
 logFile.write('Log file has created. Program has started.\n\n')
 
-print('Hi there! Let\'s quickly get down to deal. \nIf you want to find a folder that contains the greatest number of files, press 1. \nIf you want to find a folder that uses the most disk space, press 2. \nYour answer is: ')
-logFile.write('Hi there! Let\'s quickly get down to deal. \nIf you want to find a folder that contains the greatest number of files, press 1. \nIf you want to find a folder that uses the most disk space, press 2. Your answer is: ')
+while True:
+	mode = int(input('Hi there! Let\'s quickly get down to deal. \nIf you want to find a folder that contains the greatest number of files, press 1. \nIf you want to find a folder that uses the most disk space, press 2. \nYour answer is: '))
+	logFile.write('Hi there! Let\'s quickly get down to deal. \nIf you want to find a folder that contains the greatest number of files, press 1. \nIf you want to find a folder that uses the most disk space, press 2. Your answer is: \n\n')
+	if mode == 1 or mode == 2:
+		print('You have chosen ' +str(mode) + '.')
+		logFile.write('User has chosen ' + str(mode) + '.\n\n')
+		break
+	else:
+		print('Error: you must choose 1 or 2. Try Again.\n')
+		logFile.write('Error: you must choose 1 or 2. Try Again.\n')
+		continue
+		
 
-mode = input()
-print('You have chosen ' + mode + '.')
-logFile.write('User has chosen ' + mode + '.\n\n')
+
 
 
 #TODO: ask user what does he want: folder with greatest number. Store answer as boolean
